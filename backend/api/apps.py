ee5f28040708418f46c8e83cdd0b1d5499afb42f
coderslav <coderslav@gmail.com>
2022-04-30 22:34:08 +0200

    BACKEND: changed app

--- backend/api/apps.py
+++ backend/api/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class CleanAppConfig(AppConfig):
+class ApiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'clean_app'
+    name = 'api'
