de3aa384855de45406041ecd1cf4b597c978c079
coderslav <coderslav@gmail.com>
2022-04-30 20:34:12 +0200

    BACKEND: connected to PostgreSQL

--- backend/clean_app_server/settings.py
+++ backend/clean_app_server/settings.py
@@ -78,8 +78,11 @@ WSGI_APPLICATION = 'clean_app_server.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'clean_app',
+        'USER': 'coderslav',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
